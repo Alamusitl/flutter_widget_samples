929097adaf9dfc40bda88464714a92bbe4e45743
alamusi <alamusitl@163.com>
2020-06-10 15:13:55 +0800

    update

--- lib/widgets/popup_filter.dart
+++ lib/widgets/popup_filter.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Duration _kMenuDuration = Duration(milliseconds: 300);
+const Duration _kDuration = Duration(milliseconds: 300);
 const double _kMenuCloseIntervalEnd = 2.0 / 3.0;
-const double _kMenuHorizontalPadding = 16.0;
-const double _kMenuDividerHeight = 16.0;
-const double _kMenuMaxWidth = 5.0 * _kMenuWidthStep;
-const double _kMenuMinWidth = 2.0 * _kMenuWidthStep;
-const double _kMenuVerticalPadding = 8.0;
-const double _kMenuWidthStep = 56.0;
 const double _kMenuScreenPadding = 8.0;
 
 typedef PopupConfirm = void Function();
@@ -22,7 +16,7 @@ class PopupWrapper extends StatefulWidget {
     this.onConfirm,
     this.onCancel,
     this.padding = EdgeInsets.zero,
-    this.elevation,
+    this.elevation = 8.0,
     this.shape,
     this.color,
     this.tooltip,
@@ -49,22 +43,43 @@ class PopupWrapper extends StatefulWidget {
 class _PopupWrapperState extends State<PopupWrapper> {
   void showPopup() {
     final RenderBox child = context.findRenderObject() as RenderBox;
-    final RenderBox overlay =
-        Overlay.of(context).context.findRenderObject() as RenderBox;
-    print('${child.localToGlobal(Offset.zero)}');
-    print('${overlay.localToGlobal(Offset.zero)}');
-    final RelativeRect position = RelativeRect.fromRect(
+    final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
+    print(child.localToGlobal(widget.offset, ancestor: overlay));
+    print(child.localToGlobal(child.size.bottomRight(Offset.zero), ancestor: overlay));
+    print('${child.size}');
+    print('${overlay.size}');
+    print('${Rect.fromPoints(
+      child.localToGlobal(widget.offset, ancestor: overlay),
+      child.localToGlobal(child.size.bottomRight(Offset.zero), ancestor: overlay),
+    )}');
+    print('${Offset.zero & overlay.size}');
+    RelativeRect position = RelativeRect.fromRect(
       Rect.fromPoints(
         child.localToGlobal(widget.offset, ancestor: overlay),
-        child.localToGlobal(child.size.bottomRight(Offset.zero),
-            ancestor: overlay),
+        child.localToGlobal(child.size.bottomRight(Offset.zero), ancestor: overlay),
       ),
       Offset.zero & overlay.size,
+    );
+    print('$position');
+    double childY = child.localToGlobal(widget.offset, ancestor: overlay).dy;
+    position = RelativeRect.fromLTRB(
+      0.0,
+      childY + child.size.height,
+      overlay.size.width,
+      overlay.size.height,
+    );
+    print('$position');
+    Widget popupChild = Card(
+      color: widget.color,
+      shape: widget.shape,
+      elevation: widget.elevation,
+      margin: widget.padding,
+      child: widget.popupView,
     );
     Navigator.of(context)
         .push(_PopupWrapRoute(
           position: position,
-          child: widget.popupView,
+          child: popupChild,
         ))
         .then((value) => null);
   }
@@ -90,7 +105,7 @@ class _PopupWrapperState extends State<PopupWrapper> {
 class _PopupWrapRoute extends PopupRoute {
   _PopupWrapRoute({
     Key key,
-    this.position,
+    @required this.position,
     @required this.child,
   });
 
@@ -108,7 +123,7 @@ class _PopupWrapRoute extends PopupRoute {
   String barrierLabel;
 
   @override
-  Duration get transitionDuration => const Duration(milliseconds: 300);
+  Duration get transitionDuration => _kDuration;
 
   @override
   Animation<double> createAnimation() {
@@ -120,15 +135,9 @@ class _PopupWrapRoute extends PopupRoute {
   }
 
   @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    print('$position');
-    return MediaQuery.removePadding(
-      context: context,
-      removeLeft: true,
-      removeRight: true,
-      removeTop: true,
-      removeBottom: true,
+  Widget buildPage(
+      BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+    return SingleChildScrollView(
       child: Builder(builder: (context) {
         return CustomSingleChildLayout(
           delegate: _PopupWrapRouteLayout(
@@ -155,59 +164,24 @@ class _PopupWrapRouteLayout extends SingleChildLayoutDelegate {
   BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
     return BoxConstraints.loose(
       constraints.biggest -
-          const Offset(
-            _kMenuScreenPadding * 2.0,
-            _kMenuScreenPadding * 2.0,
+          Offset(
+            0.0,
+            position.top,
           ) as Size,
     );
   }
 
   @override
   Offset getPositionForChild(Size size, Size childSize) {
-    // size: The size of the overlay.
-    // childSize: The size of the menu, when fully open, as determined by
-    // getConstraintsForChild.
-
-    // Find the ideal vertical position.
+    // Find the ideal vertical & horizontal position.
+    double x = position.left;
     double y = position.top;
 
-    // Find the ideal horizontal position.
-    double x;
-    if (position.left > position.right) {
-      // Menu button is closer to the right edge, so grow to the left, aligned to the right edge.
-      x = size.width - position.right - childSize.width;
-    } else if (position.left < position.right) {
-      // Menu button is closer to the left edge, so grow to the right, aligned to the left edge.
-      x = position.left;
-    } else {
-      // Menu button is equidistant from both edges, so grow in reading direction.
-      assert(textDirection != null);
-      switch (textDirection) {
-        case TextDirection.rtl:
-          x = size.width - position.right - childSize.width;
-          break;
-        case TextDirection.ltr:
-          x = position.left;
-          break;
-      }
-    }
-
-    // Avoid going outside an area defined as the rectangle 8.0 pixels from the
-    // edge of the screen in every direction.
-    if (x < _kMenuScreenPadding)
-      x = _kMenuScreenPadding;
-    else if (x + childSize.width > size.width - _kMenuScreenPadding)
-      x = size.width - childSize.width - _kMenuScreenPadding;
-    if (y < _kMenuScreenPadding)
-      y = _kMenuScreenPadding;
-    else if (y + childSize.height > size.height - _kMenuScreenPadding)
-      y = size.height - childSize.height - _kMenuScreenPadding;
     return Offset(x, y);
   }
 
   @override
   bool shouldRelayout(_PopupWrapRouteLayout oldDelegate) {
-    return position != oldDelegate.position ||
-        textDirection != oldDelegate.textDirection;
+    return position != oldDelegate.position || textDirection != oldDelegate.textDirection;
   }
 }
